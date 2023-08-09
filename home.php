5de4d40042b185aa13f624b598d293b6797417ba
Fvst1k <87616844+Fvst1k@users.noreply.github.com>
2023-08-09 16:52:41 +0300

    add header and footer (- adaptive)

--- home.php
+++ home.php
@@ -1,10 +1,18 @@
 <?php
 /*
-Template Name: home pulsepro
+Template Name: home
 */
 ?>
 
 <?php get_header() ?>
+
+
+        <section class="first-on-page">
+            <div class="block__content block__content_first-on-page">
+
+            </div>
+        </section>
+
 
 
 <?php get_footer(); ?>
