f4e0557ae15a74df8bc80fd32ce9e2654a6e3f29
Fvst1k <87616844+Fvst1k@users.noreply.github.com>
2023-08-09 18:10:12 +0300

    add background

--- home.php
+++ home.php
@@ -6,13 +6,14 @@ Template Name: home
 
 <?php get_header() ?>
 
-
         <section class="first-on-page">
             <div class="block__content block__content_first-on-page">
+                <div class="first-on-page__bg">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/background-wave.jpg" alt="">
+                </div>
+
 
             </div>
         </section>
-
-
 
 <?php get_footer(); ?>
