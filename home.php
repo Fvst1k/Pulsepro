8fb24a13e36467f5269532b05fe11c660d69365c
Fvst1k <87616844+Fvst1k@users.noreply.github.com>
2023-08-09 20:16:32 +0300

    add block first on page (-adaptive)

--- home.php
+++ home.php
@@ -11,7 +11,20 @@ Template Name: home
             <div class="first-on-page__bg">
                 <img src="<?php echo get_template_directory_uri() ?>/assets/images/background-wave.jpg" alt="">
             </div>
-
+            <div class="first-on-page__group">
+                <h1 class="first-on-page__title">Современная домашняя IT-технологиядля удобного управления своим здоровьем</h1>
+                <div class="first-on-page__text">Проводите ежедневный чекап организма
+                    за 5 минут в день,дома у компьютерабез посещения больниц и клиникc помощью прибора PulsePro</div>
+                <button class="tl__btn tl__btn_first-on-page">
+                    <p>Партнерство с нами</p>
+                    <span class="tl__btn-arrow">
+                    <svg width="28" height="16" viewBox="0 0 28 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M26.7211 8.70711C27.1117 8.31658 27.1117 7.68342 26.7211 7.29289L20.3572 0.928932C19.9667 0.538408 19.3335 0.538408 18.943 0.928932C18.5524 1.31946 18.5524 1.95262 18.943 2.34315L24.5998 8L18.943 13.6569C18.5524 14.0474 18.5524 14.6805 18.943 15.0711C19.3335 15.4616 19.9667 15.4616 20.3572 15.0711L26.7211 8.70711ZM0 9L26.014 9V7L0 7L0 9Z"
+      fill="black"/>
+</svg>
+</span>
+                </button>
+            </div>
 
         </div>
     </section>
