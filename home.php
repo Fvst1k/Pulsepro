694dd4555332b9adb9d6de92cdfe79deb3e407ab
HaxProf <botnar.van@gmail.com>
2023-08-10 16:22:03 +0300

    slider

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ Template Name: home
 ?>
 
 <?php get_header() ?>
-
+      
     <section class="first-on-page">
         <div class="block__content block__content_first-on-page">
             <div class="first-on-page__bg">
