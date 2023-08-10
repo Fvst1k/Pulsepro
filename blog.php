c0665bbe3332b2ead790b57ea68eb0fea4153061
Fvst1k <87616844+Fvst1k@users.noreply.github.com>
2023-08-10 21:43:15 +0300

    add blog

--- blog.php
+++ blog.php
@@ -6,6 +6,48 @@ Template Name: blog
 
 <?php get_header() ?>
 
-<section></section>
+    <section class="blog">
+        <div class="block__content block__content_blog">
+            <h2 class="blog__title b-line_title">Блог</h2>
+            <div class="blog__table">
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+                <a class="blog__card" href="#">
+                    <div class="blog__img"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog-1.png" alt="error"></div>
+                    <time class="blog__date">06.08.2023</time>
+                    <h3 class="blog__card-title">Фитнес-браслеты и часы не измеряют вариабельность сердечного ритма! Неужели всё это — обман?</h3>
+                    <p class="blog__card-subtitle">Что вообще такое вариабельность сердечного ритма? Почему всё — обман? И какое отношение к этому показателю имеют все фитнес-трекеры?</p>
+                </a>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
